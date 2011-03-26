b48f778be3eef84226388659f2b1f3348d423f1f
Tony Zito <tonyzito@gmail.com>
2011-03-26 13:03:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dream3::Application.config.secret_token = 'd3d22ea1e7529868b2e89050521023a1638a7bff284606d974ea040724b717073a44d5ded5256a0987bfe8c6db3508f65f61290874c9e0373b1007d1b862226b'
