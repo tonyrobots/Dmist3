2451013828ccd2382cfdc521038be264c76b3a10
Tony Zito <tonyzito@gmail.com>
2011-05-13 17:42:15 -0700

    a little more view work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @dreams = Dream.where(:visible => TRUE, :private => FALSE)
+    @dreams = Dream.where(:visible => TRUE, :private => FALSE).limit(5)
   end
   
 end
