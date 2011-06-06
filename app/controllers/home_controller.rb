f6206cdf400ffca2f295bbc1a562a3004471c737
Tony Zito <tonyzito@gmail.com>
2011-06-06 10:58:09 -0700

    some cosmetic changes, and added 'weather'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
     @dreams = Dream.where(:visible => TRUE, :private => FALSE).limit(5)
     # TODO add featured dream logic
     @featured_dream = Dream.first
+
+    feelings = Dream.find(:all, :limit=> 30, :conditions => {:visible => TRUE}, :select => :feeling).map(&:feeling)
+    @feeling_median = feelings.sort[feelings.length/2]
+    @feeling_mean = feelings.instance_eval { reduce(:+) / size.to_f }
   end
-  
+
 end
