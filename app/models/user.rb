1e30f384f1672b37c093e020f20c29c2d80ca215
Tony Zito <tonyzito@gmail.com>
2011-09-08 13:01:19 -0700

    fixed little role bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
+  
+  # Role definitions
+  ADMIN = 1
+  USER  = 0
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-
+  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :username, :password, :password_confirmation, :remember_me, :avatar, :location, :role
   
