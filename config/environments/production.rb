1abe21a525d6f2619c356e351f78fe8445fcc004
Tony Zito <tonyzito@gmail.com>
2012-04-26 11:47:01 -0700

    tweaking production sendmail set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Dream3::Application.configure do
   config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
-    :domain => ENV['SENDGRID_DOMAIN'],
+    :domain => "dreamerist.com",
     :user_name =>  ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :authentication => 'plain',
