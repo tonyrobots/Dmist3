1be040e2483dbfc1bc13b4a5c56d1196be3523f3
Tony Zito <tonyzito@gmail.com>
2012-04-26 10:59:38 -0700

    got mailer working on development, nice-ified footer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,17 @@ Dream3::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   :location => '/usr/sbin/sendmail',
+  #   :arguments => '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
+  
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -33,5 +43,8 @@ Dream3::Application.configure do
   config.assets.debug = true
   
   config.serve_static_assets = true
+  
+  #email config
+ 
 end
 
