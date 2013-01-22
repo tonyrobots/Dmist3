26150d00d2f2ed924bbbe08f2b85cb721519a328
Tony Zito <tonyzito@gmail.com>
2013-01-21 16:01:55 -0800

    fixed up static controller and about page, removed ugly shadow on type

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,23 @@
 # app/controllers/static_controller.rb
 class StaticController < ApplicationController
-  layout 'plain'
+  #layout 'plain'
   skip_before_filter :check_invite_code
+  
 
   caches_page :show
 
   def show
+    #some page-specific bits
+    case params[:path]
+    when "about"
+      @title = "Dreamerist FAQ"
+    when "splash"
+      layout = 'plain'
+    end    
+    
     valid = %w(splash about)
     if valid.include?(params[:path])
-      render :template => File.join('static', params[:path])
+      render :template => File.join('static', params[:path]), :layout => layout || 'application'
     else
       render :file   => File.join(Rails.root, 'public', '404.html'), 
              :status => 404
